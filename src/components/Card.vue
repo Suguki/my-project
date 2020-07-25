9c55f874bf750cab35fd09d2c75c4ffe4aef3ea3
suguki <suguki.cr@gmail.com>
2020-07-25 21:08:55 +0900

    カードコンポーネントを作成.

---
+++ src/components/Card.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="profile-item card text-center" ontouchstart @click="reverse()">
+      <div v-if="!is_reversed">
+          <div class="prifile-title card-header">
+              {{ title }}
+          </div>
+          <div class="profile-body card-body p-2">
+              {{ description }}
+          </div>
+      </div>
+
+    <div v-else>
+        <div class="profile-title card-header text-info">
+            {{ title}}
+        </div>
+        <div class="profile-body card-body p-2">
+            {{ comment }}
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Card',
+    props: {
+        title: String,
+        description: String,
+        comment: String
+    },
+    data() {
+        return {
+            is_reversed: false
+        }
+    },
+    methods: {
+        reverse() {
+            this.is_reversed = !this.is_reversed;
+        }
+    }
+}
+</script>
+
+<style scoped>
+  .profile-item {
+    filter: drop-shadow(0 5px 10px rgba(0, 0, 0, 0.6));
+    margin-top: 10px;
+  }
+  .profile-item:active {
+    background-color: rgb(102, 102, 102);
+  }
+</style>
