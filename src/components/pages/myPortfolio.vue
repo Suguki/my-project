40d2dfa932f23b14b3a7053f01897620a4265488
suguki <suguki.cr@gmail.com>
2020-08-21 14:55:56 +0900

    my portfolioにて、pig_gameリポジトリへのリンクを追加 + フォーマット。

--- src/components/pages/myPortfolio.vue
+++ src/components/pages/myPortfolio.vue
@@ -17,12 +17,19 @@
           <b-modal id="PigGameModal" title="PigGame">
             <div class="my-4">
               <ul>
-                <li><span class="font-weight">概要：</span>二人でサイコロを振り、どちらが早く出た目の合計が100になるか競うゲーム。意外とゲーム性あります。</li>
-                <li><span class="font-weight">使用言語：</span>JavaScript/HMTL/CSS<br>
-                DOM操作を利用し、ページ遷移することなくアクションを実行するため、JavaScriptを使用し作成しました。
+                <li>
+                  <span class="font-weight">概要：</span>二人でサイコロを振り、どちらが早く出た目の合計が100になるか競うゲーム。意外とゲーム性あります。
                 </li>
-                <li><span class="font-weight">使用環境：</span>HTML＋JSファイルをブラウザで読みこんで使用します。</li>
-                <li><a href="#">Githubページへ</a></li>
+                <li>
+                  <span class="font-weight">使用言語：</span>JavaScript/HMTL/CSS
+                  <br />DOM操作を利用し、ページ遷移することなくアクションを実行するため、JavaScriptを使用し作成しました。
+                </li>
+                <li>
+                  <span class="font-weight">使用環境：</span>HTML＋JSファイルをブラウザで読みこんで使用します。
+                </li>
+                <li>
+                  <a target="_blank" href="https://github.com/Suguki/pig_game">Githubページへ</a>
+                </li>
               </ul>
             </div>
           </b-modal>
@@ -43,20 +50,28 @@
           <b-modal id="KyodokoikuModal" title="今日どこ行く？">
             <div class="my-4">
               <ul>
-                <li><span class="font-weight">概要：</span>休日何かしたいけど何しようかな...そんなときはこのアプリで案を出すのもいいのではないでしょうか。
-                気分に沿って案を出してくれます。割と細かく案を絞れます。</li>
-                <li><span class="font-weight">使用言語：</span>HTML/CSS/Bootstrap/PHP<br>
-                アプリの機能を実装するに当たり、DBを使ったバックエンド処理が必要であったためphpを使用し作成しました。</li>
-                <li><span class="font-weight">使用環境：</span>Docker/MySQL/nginx</li>
-                <li><a target="_blank" href="https://github.com/Suguki/kyodokoiku">Githubページへ</a></li>
+                <li>
+                  <span class="font-weight">概要：</span>休日何かしたいけど何しようかな...そんなときはこのアプリで案を出すのもいいのではないでしょうか。
+                  気分に沿って案を出してくれます。割と細かく案を絞れます。
+                </li>
+                <li>
+                  <span class="font-weight">使用言語：</span>HTML/CSS/Bootstrap/PHP
+                  <br />アプリの機能を実装するに当たり、DBを使ったバックエンド処理が必要であったためphpを使用し作成しました。
+                </li>
+                <li>
+                  <span class="font-weight">使用環境：</span>Docker/MySQL/nginx
+                </li>
+                <li>
+                  <a target="_blank" href="https://github.com/Suguki/kyodokoiku">Githubページへ</a>
+                </li>
               </ul>
             </div>
           </b-modal>
         </b-card>
       </div>
     </div>
-    <br>
-    <br>
+    <br />
+    <br />
     <div class="card-deck row">
       <div class="col-md-6 text-center">
         <b-card
@@ -73,12 +88,20 @@
           <b-modal id="TripCoffeeModal" title="TripCoffee">
             <div class="my-4">
               <ul>
-                <li><span class="font-weight">概要：</span>カフェ好きが高じて誕生しました.今日どこ行く？</li>
+                <li>
+                  <span class="font-weight">概要：</span>カフェ好きが高じて誕生しました.今日どこ行く？
+                </li>
                 <p>とは違いレビューや店の評価ができます。</p>
-                <li><span class="font-weight">使用言語：</span>HTML/CSS/Bootstrap/PHP/Laravel
-                <br>今日どこ行く？を作成し、phpの基礎をある程度学んだので、フレームワークを利用しもう少し機能的なアプリを作成したいと思いLaravelを用いて作成しました。</li>
-                <li><span class="font-weight">使用環境：</span> Docker/MySQL/nginx</li>
-                <li><a href="https://github.com/Suguki/cafe_review" target="_blank">Githubページへ</a></li>
+                <li>
+                  <span class="font-weight">使用言語：</span>HTML/CSS/Bootstrap/PHP/Laravel
+                  <br />今日どこ行く？を作成し、phpの基礎をある程度学んだので、フレームワークを利用しもう少し機能的なアプリを作成したいと思いLaravelを用いて作成しました。
+                </li>
+                <li>
+                  <span class="font-weight">使用環境：</span> Docker/MySQL/nginx
+                </li>
+                <li>
+                  <a href="https://github.com/Suguki/cafe_review" target="_blank">Githubページへ</a>
+                </li>
               </ul>
             </div>
           </b-modal>
